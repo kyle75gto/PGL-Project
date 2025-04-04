821401c04790fa302ad9fae6333c44ff7312e9e0
Kylian Yamami <kylian.yamami@edu.devinci.fr>
2025-04-04 22:31:05 +0200

    scraping réussi

--- scraper_velib.sh
+++ scraper_velib.sh
@@ -13,13 +13,23 @@ if [ ! -s "$HTML_FILE" ]; then
 fi
 
 # Extraire les identifiants (5 chiffres uniquement)
-grep -oP '<span title="\K\d{4,5}(?=" dir="auto")' "$HTML_FILE" > temp_ids.txt
+
+grep -oP '<span title="\K\d{4,5}(?=" dir="auto")' "$HTML_FILE" > ids.txt
+
+grep -oP '<span title="\K\d+(?=" dir="auto">)' velib_page.html | sed -n '2~6p' > bornettes.txt
+
+grep -oP '<span title="\K\d+(?=" dir="auto">)' velib_page.html | sed -n '3~6p' > total_velos.txt
+
+grep -oP '<span title="\K\d+(?=" dir="auto">)' velib_page.html | sed -n '4~6p' > velos_mecaniques.txt
+
+grep -oP '<span title="\K\d+(?=" dir="auto">)' velib_page.html | sed -n '5~6p' > velos_electriques.txt
 
 # Créer le fichier CSV
-echo "Identifiant_Station" > "$CSV_FILE"
-cat temp_ids.txt >> "$CSV_FILE"
 
-rm temp_ids.txt
+echo "Identifiant_Station,Bornettes_Libres,Total_Velos_Dispo,Velos_Mecaniques,Velos_Electriques" > "$CSV_FILE"
+paste -d ',' ids.txt bornettes.txt total_velos.txt velos_mecaniques.txt velos_electriques.txt >> "$CSV_FILE"
+
+rm ids.txt bornettes.txt total_velos.txt velos_mecaniques.txt velos_electriques.txt
 
 echo " Fichiers générés :"
 echo "   - HTML : $HTML_FILE"
