087942f3481eb085c7b5c5912c690b3e5c9acd76
Tommy RIBEIRO <tommy.ribeiro@edu.devinci.fr>
2025-04-07 18:11:20 +0200

    Modifications for AWS

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script shell secondaire
+cd "C:\Users\jqygn\Documents\ESILV\A4\S8\0.Adv. Python, Git, Linux for Bl\Project Git\PGL-Project"
+
+# Script Python
+python app.py &
+
+./scrapper_velib.sh
